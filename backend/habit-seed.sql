9b8dfca338716e7438a3bfae2671322fb9c41a8b
Margaret Sun <hi.margaret.sun@gmail.com>
2023-02-09 14:03:33 -0800

    db works

--- backend/habit-seed.sql
+++ backend/habit-seed.sql
@@ -3,3 +3,6 @@ VALUES ('testuser1', 'password');
 
 INSERT INTO habits (habit_name, weekly_goal, username)
 VALUES ('testhabit1', 5, 'testuser1');
+
+INSERT INTO habit_log(username, habit_id)
+VALUES('testuser1', 1)
